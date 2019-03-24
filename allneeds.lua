f0dbb1c78ee74ebc65fa91c10c998379dc7ac57b
Peter Hansen <previsualconsent@gmail.com>
2019-03-23 21:35:47 -0500

    cleanup and comments

--- allneeds.lua
+++ allneeds.lua
@@ -6,10 +6,6 @@ allneeds
 Show which neesd are high for all dwarfs
 
 ]====]
-local utils = require('utils')
-local validArgs = utils.invert({})
-local args = utils.processArgs({...}, validArgs)
-
 local ENUM = {}
 
 ENUM[0]  = "Socialize"
@@ -45,39 +41,39 @@ ENUM[29] = "AdmireArt"
 
 
 local need = {}
-local focus = {}
 local n = 0
-local n_needs = 0
 for _, unit in ipairs(df.global.world.units.all) do
-    -- local unit = dfhack.gui.getSelectedUnit(true)
     if not unit then
-        qerror('A unit must be specified or selected.')
+      qerror('Unit not real.')
     end
+    -- Select valid dwarf
     if unit.status.current_soul and unit.race == 572 then
         n = n+1
-         local mind = unit.status.current_soul.personality.needs
-          for k,v in pairs(mind) do
-              n_needs = n_needs +1
-              if need[v.id] == nil then
-                  need[v.id] = {0, 0}
-              end
-              need[v.id] = {need[v.id][1] + v.need_level, need[v.id][2] + v.focus_level}
-          end
+        local mind = unit.status.current_soul.personality.needs
+        -- sum need_level and focus_level for each need
+        for k,v in pairs(mind) do
+            if need[v.id] == nil then
+                need[v.id] = {0, 0}
+            end
+            need[v.id] = {need[v.id][1] + v.need_level, need[v.id][2] + v.focus_level}
+        end
     end
-    -- break
 end
 
+-- for sorting output
 function compare(a,b)
     return a[1]*a[2] > b[1]*b[2]
 end
+
 sorted = {}
 i = 1
 for k,v in pairs(need) do
     sorted[i] = {v[1], v[2], ENUM[k]}
     i = i + 1
 end
-
 table.sort(sorted, compare)
+
+-- Print sorted output
 print(string.format("%20s %8s %8s", "Need", "Weight", "Focus"))
 for k,v in ipairs(sorted) do
     print(string.format("%20s %8.1f %8.1f",  v[3], v[1]/n, v[2]/n))
