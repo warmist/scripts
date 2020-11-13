5cf9850a34285c795c14feeb936bf9c7e4f64694
myk002 <myk002@yahoo.com>
2020-11-13 10:21:28 -0800

    rewrite unsuspend and autounsuspend in lua
    so we can properly check for buildingplan-managed buildings

---
+++ autounsuspend.lua
@@ -0,0 +1,34 @@
+# automate periodic running of the unsuspend script
+--[====[
+
+autounsuspend
+=============
+Periodically check construction jobs and keep them unsuspended with the
+`unsuspend` script.
+]====]
+
+local job_name = '__autounsuspend'
+
+local function help()
+    print('syntax: autounsuspend [start|stop]')
+end
+
+local function stop()
+    dfhack.run_command{'repeat', '-cancel', job_name}
+    print('autounsuspend Stopped.')
+end
+
+local function start()
+    dfhack.run_command{'repeat', '-name', job_name, '-time', '1',
+                       '-timeUnits', 'days', '-command', '[', 'unsuspend', ']'}
+    print('autounsuspend Running.')
+end
+
+local action_switch = {
+    start=start,
+    stop=stop,
+}
+setmetatable(action_switch, {__index=function() return help end})
+
+local args = {...}
+action_switch[table.remove(args, 1) or 'help']()
