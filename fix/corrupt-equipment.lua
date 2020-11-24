234c0087ba993451098ca209c8cb84a299cbfaa0
lethosor <lethosor@gmail.com>
2020-11-24 00:23:20 -0500

    Restore check/removal of items of the wrong type in equipment vectors
    In https://dffd.bay12games.com/file.php?id=14213, removing items of the wrong
    type immediately when the save is loaded appears to prevent the invalid
    pointers from being added to the vectors at all. (Once invalid pointers are
    present, i.e. after running the save for a few ticks without running the script,
    they must also be removed to prevent the crash.)
    
    Ref dfhack/dfhack#1678

--- fix/corrupt-equipment.lua
+++ fix/corrupt-equipment.lua
@@ -11,11 +11,15 @@ Fixes some corruption that can occur in equipment lists, as in :bug:`11014`.
 
 local utils = require("utils")
 
-function fix_vector(vec, valid_items, message)
+function fix_vector(vec, valid_items, valid_type, vector_desc)
   local raw_vec = df.reinterpret_cast("ptr-vector", vec)
   for i = #vec - 1, 0, -1 do
     if not valid_items[utils.addressof(raw_vec[i])] then
-      dfhack.printerr(string.format("%s (index %i)", message, i))
+      print(string.format("Removing corrupted %s (index %i)", vector_desc, i))
+      vec:erase(i)
+    elseif valid_type and not valid_type:is_instance(vec[i]) then
+      print(string.format("Removing %s of invalid type %s (index %i)",
+        vector_desc, df.item_type[vec[i]:getType()], i))
       vec:erase(i)
     end
   end
@@ -27,26 +31,27 @@ function fix_equipment ()
     valid_items[utils.addressof(item)] = true
   end
 
-  local categories =
-    {{"FLASK", df.item_flaskst},
-     {"WEAPON", df.item_weaponst},
-     {"ARMOR", df.item_armorst},
-     {"SHOES", df.item_shoesst},
-     {"SHIELD", df.item_shieldst},
-     {"HELM", df.item_helmst},
-     {"GLOVES", df.item_glovesst},
-     {"AMMO", df.item_ammost},
-     {"PANTS", df.item_pantsst},
-     {"BACKPACK", df.item_backpackst},
-     {"QUIVER", df.item_quiverst}}
+  local valid_types = {
+    FLASK = df.item_flaskst,
+    WEAPON = df.item_weaponst,
+    ARMOR = df.item_armorst,
+    SHOES = df.item_shoesst,
+    SHIELD = df.item_shieldst,
+    HELM = df.item_helmst,
+    GLOVES = df.item_glovesst,
+    AMMO = df.item_ammost,
+    PANTS = df.item_pantsst,
+    BACKPACK = df.item_backpackst,
+    QUIVER = df.item_quiverst,
+  }
 
   for category in pairs(df.global.ui.equipment.items_unmanifested) do
     fix_vector(df.global.ui.equipment.items_unmanifested[category], valid_items,
-      "Removing corrupted unmanifested " .. category)
+      valid_types[category], "unmanifested " .. category)
     fix_vector(df.global.ui.equipment.items_unassigned[category], valid_items,
-      "Removing corrupted unassigned " .. category)
+      valid_types[category], "unassigned " .. category)
     fix_vector(df.global.ui.equipment.items_assigned[category], valid_items,
-      "Removing corrupted assigned " .. category)
+      valid_types[category], "assigned " .. category)
   end
 
   for i, squad in ipairs (df.global.world.squads.all) do
@@ -57,8 +62,6 @@ function fix_equipment ()
       end
       squad_name = dfhack.df2utf(squad_name)
 
---      dfhack.println (squad_name, i)
-
       for k, position in ipairs (squad.positions) do
         for l, item_id in ipairs (position.assigned_items) do
           local legal_type_found = false
@@ -66,16 +69,10 @@ function fix_equipment ()
 
           -- the item itself may not be present - e.g. items belonging to military units on raids
           if item then
-            for m, element in ipairs (categories) do
-              if item._type == element [2] then
-                legal_type_found = true
-                break
-              end
-            end
-
-            if not legal_type_found then
+            local item_type_str = tostring(df.item_type[item:getType()])
+            if not valid_types[item_type_str] then
               dfhack.printerr ("Item " .. tostring (l) .. " assigned to squad member " .. tostring (k) .. " of squad " .. squad_name ..
-                               " is of unexpected type " .. tostring (item._type) ..  ". Detection only. No action performed.")
+                               " is of unexpected type " .. item_type_str ..  ". Detection only. No action performed.")
             end
           end
         end
