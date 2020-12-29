9437095e0596cb800fc60304d3a430a67f0cf486
Timur Kelman <tymur.gubayev@gmail.com>
2020-12-29 14:57:50 +0100

    fix "Title underline too short"

--- gui/workorder-recheck.lua
+++ gui/workorder-recheck.lua
@@ -2,7 +2,7 @@
 --[====[
 
 gui/workorder-recheck
-====================
+=====================
 Sets the status to ``Checking`` (from ``Active``) of the selected work order (in the j-m or u-m screens). This makes the manager reevaluate its conditions.
 
 Example keybinding (put it in your ``dfhack*.init``-file):
