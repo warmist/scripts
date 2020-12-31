3cc0e295ada601931c1086250e53627b2f168973
lethosor <lethosor@gmail.com>
2020-12-31 12:37:45 -0500

    Move out of gui folder

--- workorder-recheck.lua
+++ workorder-recheck.lua
@@ -1,8 +1,8 @@
 -- Resets the selected work order to the `Checking` state
 --[====[
 
-gui/workorder-recheck
-=====================
+workorder-recheck
+=================
 Sets the status to ``Checking`` (from ``Active``) of the selected work order (in the ``j-m`` or ``u-m`` screens).
 This makes the manager reevaluate its conditions.
 ]====]
