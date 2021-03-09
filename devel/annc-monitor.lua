19f84c9a8f4acac933d65c8dfe5e396f4caf84bd
Ben Lubar <ben.lubar@gmail.com>
2021-03-09 14:26:43 -0600

    devel/annc-monitor: add option to filter out combat reports

--- devel/annc-monitor.lua
+++ devel/annc-monitor.lua
@@ -8,20 +8,25 @@ Displays announcements and reports in the console.
 
 :enable|start:      Begins monitoring
 :disable|stop:      Stops monitoring
+:report enable:     Show combat reports
+:report disable:    Only show announcements
 
 ]====]
 
-VERSION = '0.2'
+VERSION = '0.3'
 
 local eventful = require 'plugins.eventful'
 
 enabled = enabled or false
+filter = filter or false
 
 function usage()
     print [[
 Usage:
     annc-monitor start|enable: Begin monitoring
     annc-monitor stop|disable: End monitoring
+    annc-monitor report enable: Show combat reports
+    annc-monitor report disable: Only show announcements
 ]]
 end
 
@@ -40,6 +45,14 @@ if #args >= 1 then
         enabled = true
     elseif args[1] == 'stop' or args[1] == 'disable' then
         enabled = false
+    elseif args[1] == 'report' and #args >= 2 then
+        if args[2] == 'enable' then
+            filter = false
+        elseif args[2] == 'disable' then
+            filter = true
+        else
+            usage()
+        end
     else
         usage()
     end
@@ -51,6 +64,9 @@ eventful.enableEvent(eventful.eventType.REPORT, 1)
 function eventful.onReport.annc_monitor(id)
     if enabled and dfhack.isWorldLoaded() then
         local annc = df.report.find(id)
+        if filter and not annc.flags.announcement then
+            return
+        end
         local color = annc.color + (annc.bright and 8 or 0)
         log(annc.text, color)
     end
