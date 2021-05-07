69cf0498e6a1ff07339a62b084afb23b6fd0b528
myk002 <myk002@yahoo.com>
2021-05-07 15:35:33 -0700

    add dummy gui/blueprint so plugin docs will build

---
+++ gui/blueprint.lua
@@ -0,0 +1,22 @@
+-- A GUI front-end for the blueprint plugin
+--@ module = true
+--[====[
+
+gui/blueprint
+=============
+The `blueprint` plugin records the structure of a portion of your fortress in
+a blueprint file that you (or anyone else) can later play back with `quickfort`.
+
+This script provides a visual, interactive interface to make configuring and
+using the blueprint plugin much easier.
+
+Usage:
+
+    gui/blueprint [<name> [<phases>]] [<options>]
+
+All parameters are optional. Anything you specify will override the initial
+values set in the interface. See the `blueprint` documentation for information
+on the possible parameters and options.
+]====]
+
+print('coming soon!')
