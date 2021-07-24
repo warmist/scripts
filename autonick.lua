4c9ea8586aadfe3340983ca6ef9b518fc61b552c
Susan <su@angel-island.zone>
2021-07-24 20:13:30 +0100

    spaces not tabs

--- autonick.lua
+++ autonick.lua
@@ -24,34 +24,34 @@ path = dfhack.getDFPath () .. "/dfhack-config/autonick.txt";
 
 -- grab list, put in array
 for line in io.lines(path) do
-	if line ~= "" then
-		names [index] = line;
-		index = (index + 1);
-	end;
+    if line ~= "" then
+        names [index] = line;
+        index = (index + 1);
+    end;
 end;
 
 --check current nicknames
 for _,unit in ipairs(df.global.world.units.active) do
-	if dfhack.units.isCitizen(unit) and
-	unit.name.nickname ~= "" then
-		for i,name in ipairs(names) do
-			if name == unit.name.nickname then
-				--remove current nickname from array
-				table.remove(names, i);
-			end;
-		end;
-	end;
+    if dfhack.units.isCitizen(unit) and
+    unit.name.nickname ~= "" then
+        for i,name in ipairs(names) do
+            if name == unit.name.nickname then
+                --remove current nickname from array
+                table.remove(names, i);
+            end;
+        end;
+    end;
 end;
 
 --assign names
 for _,unit in ipairs(df.global.world.units.active) do
-	if #names > 0 then --if there are any names left
-		if dfhack.units.isCitizen(unit) and
-		unit.name.nickname == "" then
-			newnameIndex = math.random (#names);
-			dfhack.units.setNickname(unit, names[newnameIndex]);
-			table.remove(names, newnameIndex);
-		end;
-	end;
+    if #names > 0 then --if there are any names left
+        if dfhack.units.isCitizen(unit) and
+        unit.name.nickname == "" then
+            newnameIndex = math.random (#names);
+            dfhack.units.setNickname(unit, names[newnameIndex]);
+            table.remove(names, newnameIndex);
+        end;
+    end;
 end;
 
