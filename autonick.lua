d58e3dfef76f7314c817f6f6b460d3a21bcd6a2b
Susan <su@angel-island.zone>
2021-07-24 21:32:00 +0100

    no more semicolons

--- autonick.lua
+++ autonick.lua
@@ -18,17 +18,17 @@ You may wish to use this script with the "repeat" command, e.g:
 
 ]====]
 
-names = {};
+names = {}
 
 path = dfhack.getDFPath () .. "/dfhack-config/autonick.txt";
 
 -- grab list, put in array
 for line in io.lines(path) do
-    line = line:trim();
+    line = line:trim()
     if (line ~= "") and (not line:startswith('#')) then
-        table.insert(names, line);
-    end;
-end;
+        table.insert(names, line)
+    end
+end
 
 --check current nicknames
 for _,unit in ipairs(df.global.world.units.active) do
@@ -37,23 +37,23 @@ for _,unit in ipairs(df.global.world.units.active) do
         for i,name in ipairs(names) do
             if name == unit.name.nickname then
                 --remove current nickname from array
-                table.remove(names, i);
-            end;
-        end;
-    end;
-end;
+                table.remove(names, i)
+            end
+        end
+    end
+end
 
 --assign names
 for _,unit in ipairs(df.global.world.units.active) do
-    if #names <= 0 then break end;
+    if #names <= 0 then break end
 
     --if there are any names left
     if dfhack.units.isCitizen(unit) and
     unit.name.nickname == "" then
-        newnameIndex = math.random (#names);
-        dfhack.units.setNickname(unit, names[newnameIndex]);
-        table.remove(names, newnameIndex);
-        end;
-    end;
-end;
+        newnameIndex = math.random (#names)
+        dfhack.units.setNickname(unit, names[newnameIndex])
+        table.remove(names, newnameIndex)
+        end
+    end
+end
 
