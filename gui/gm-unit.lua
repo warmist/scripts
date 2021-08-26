6eb3790a0407d9c7b9ae9cfeed6850820171b89a
Andrea Cattaneo <andrea.cattaneo.88@gmail.com>
2021-08-26 20:43:36 +0200

    Extract skill editor.

--- gui/gm-unit.lua
+++ gui/gm-unit.lua
@@ -1,4 +1,5 @@
 -- Interface powered, user friendly, unit editor
+--@ module = true
 
 --[====[
 
@@ -17,6 +18,11 @@ local setbelief = dfhack.reqscript("modtools/set-belief")
 local setpersonality = dfhack.reqscript("modtools/set-personality")
 local setneed = dfhack.reqscript("modtools/set-need")
 local setorientation = dfhack.reqscript("set-orientation")
+
+Editor = defclass(Editor, gui.FramedScreen)
+Editor.ATTRS = {
+    frame_style = gui.GREY_LINE_FRAME
+}
 
 rng = rng or dfhack.random.new(nil, 10)
 
@@ -62,122 +68,8 @@ end
 
 
 -------------------------------various subeditors---------
---TODO set local should or better yet skills vector to reduce long skill list access typing
-editor_skills = defclass(editor_skills, gui.FramedScreen)
-editor_skills.ATTRS = {
-    frame_style = gui.GREY_LINE_FRAME,
-    frame_title = "Skill editor",
-    target_unit = DEFAULT_NIL,
-    learned_only = false,
-}
-function list_skills(unit,learned_only)
-    local u_skills = unit.status.current_soul.skills
-    local ret = {}
-    for skill,v in ipairs(df.job_skill) do
-        if skill ~= df.job_skill.NONE then
-            local u_skill = utils.binsearch(u_skills, skill, "id")
-            if u_skill or not learned_only then
-                if not u_skill then
-                    u_skill={rating=-1,experience=0}
-                end
-
-                local rating
-                if u_skill.rating >=0 then
-                    rating=df.skill_rating.attrs[u_skill.rating]
-                else
-                    rating={caption="<unlearned>",xp_threshold=0}
-                end
-
-                local text=string.format("%s: %s %d %d/%d",
-                    df.job_skill.attrs[skill].caption,
-                    rating.caption,u_skill.rating,
-                    u_skill.experience,rating.xp_threshold)
-                table.insert(ret,{
-                    text=text,
-                    id=skill,
-                    search_key=text:lower()
-                })
-            end
-        end
-    end
-    return ret
-end
-function editor_skills:update_list(no_save_place)
-    local skill_list=list_skills(self.target_unit,self.learned_only)
-    if no_save_place then
-        self.subviews.skills:setChoices(skill_list)
-    else
-        self.subviews.skills:setChoices(skill_list,self.subviews.skills:getSelected())
-    end
-end
-function editor_skills:init( args )
-    if self.target_unit.status.current_soul==nil then
-        qerror("Unit does not have soul, can't edit skills")
-    end
-
-    local skill_list=list_skills(self.target_unit,self.learned_only)
-
-    self:addviews{
-        widgets.FilteredList{
-            choices=skill_list,
-            frame = {t=0, b=1,l=1},
-            view_id="skills",
-        },
-        widgets.Label{
-            frame = { b=0,l=1},
-            text ={{text= ": exit editor ",
-                key  = "LEAVESCREEN",
-                on_activate= self:callback("dismiss")
-                },
-                {text=": remove level ",
-                key = "SECONDSCROLL_UP",
-                on_activate=self:callback("level_skill",-1)},
-                {text=": add level ",
-                key = "SECONDSCROLL_DOWN",
-                on_activate=self:callback("level_skill",1)}
-                ,
-                {text=": show learned only ",
-                key = "CHANGETAB",
-                on_activate=function ()
-                    self.learned_only=not self.learned_only
-                    self:update_list(true)
-                end}
-            }
-        },
-    }
-end
-function editor_skills:get_cur_skill()
-    local list_wid=self.subviews.skills
-    local _,choice=list_wid:getSelected()
-    if choice==nil then
-        qerror("Nothing selected")
-    end
-    local u_skill=utils.binsearch(self.target_unit.status.current_soul.skills,choice.id,"id")
-    return choice,u_skill
-end
-function editor_skills:level_skill(lvl)
-    local sk_en,sk=self:get_cur_skill()
-    if lvl >0 then
-        local rating
-
-        if sk then
-            rating=sk.rating+lvl
-        else
-            rating=lvl-1
-        end
-
-        utils.insert_or_update(self.target_unit.status.current_soul.skills, {new=true, id=sk_en.id, rating=rating}, 'id') --TODO set exp?
-    elseif sk and sk.rating==0 and lvl<0 then
-        utils.erase_sorted_key(self.target_unit.status.current_soul.skills,sk_en.id,"id")
-    elseif sk and lvl<0 then
-        utils.insert_or_update(self.target_unit.status.current_soul.skills, {new=true, id=sk_en.id, rating=sk.rating+lvl}, 'id') --TODO set exp?
-    end
-    self:update_list()
-end
-function editor_skills:remove_rust(skill)
-    --TODO
-end
-add_editor(editor_skills)
+editor_skills = reqscript("gui/editor_skills")
+add_editor(editor_skills.Editor_Skills)
 
 ------- civ editor
 RaceBox = defclass(RaceBox, dialog.ListBox)
