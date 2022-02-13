e262a8bbb1460e0fcadf8fa8e2ca5ea0b70417b7
myk002 <myk002@yahoo.com>
2022-02-12 18:00:20 -0800

    support repeat() marker in meta blueprints

--- internal/quickfort/meta.lua
+++ internal/quickfort/meta.lua
@@ -6,7 +6,10 @@ if not dfhack_flags.module then
 end
 
 local quickfort_command = reqscript('internal/quickfort/command')
+local quickfort_common = reqscript('internal/quickfort/common')
 local quickfort_parse = reqscript('internal/quickfort/parse')
+
+local log = quickfort_common.log
 
 -- blueprints referenced by meta blueprints must have a label, even if it's just
 -- a default numeric label. this is so we can provide intelligent error messages
@@ -18,7 +21,7 @@ local quickfort_parse = reqscript('internal/quickfort/parse')
 -- the lables are in ".csv format" even if they were fully specified for source
 -- .xlsx files, but those .xlsx files have since been serialized to .csv.
 local function get_section_name(cell, text, cur_sheet_name)
-    local sheet_name, label = quickfort_parse.parse_section_name(text)
+    local sheet_name, label, extra = quickfort_parse.parse_section_name(text)
     if not label then
         local cell_msg = string.format(
             'malformed blueprint section name in cell %s', cell)
@@ -33,7 +36,7 @@ local function get_section_name(cell, text, cur_sheet_name)
     end
     if not sheet_name then sheet_name = cur_sheet_name or '' end
     if not cur_sheet_name then sheet_name = '' end
-    return string.format('%s/%s', sheet_name, label)
+    return string.format('%s/%s', sheet_name, label), extra
 end
 
 local function do_meta(zlevel, grid, ctx)
@@ -41,15 +44,25 @@ local function do_meta(zlevel, grid, ctx)
     stats.meta_blueprints = stats.meta_blueprints or
             {label='Blueprints applied', value=0, always=true}
 
-    -- ensure we process blueprints in the declared order
-    local saved_zlevel = ctx.cursor.z
+    -- use get_ordered_grid_cells() to ensure we process blueprints in exactly
+    -- the declared order (pairs() over the grid makes no such guarantee)
     for _, cell in ipairs(quickfort_parse.get_ordered_grid_cells(grid)) do
-        local section_name =
+        local section_name, extra =
                 get_section_name(cell.cell, cell.text, ctx.sheet_name)
-        print(string.format('applying blueprint: "%s"', section_name))
-        quickfort_command.do_command_section(ctx, section_name)
-        ctx.cursor.z = saved_zlevel
-        stats.meta_blueprints.value = stats.meta_blueprints.value + 1
+        local modifiers =
+                quickfort_parse.get_meta_modifiers(extra, ctx.blueprint_name)
+        local repeat_str = ''
+        if modifiers.repeat_count > 1 then
+            repeat_str = (' (%d times %sward)')
+                    :format(modifiers.repeat_count,
+                            (modifiers.repeat_zoff > 0) and 'up' or 'down')
+        end
+        log('applying blueprint%s: "%s"', repeat_str, section_name)
+        quickfort_command.do_command_section(ctx, section_name, modifiers)
+        stats.meta_blueprints.value =
+                stats.meta_blueprints.value + modifiers.repeat_count
+        -- don't let called blueprints permanently alter the z-level
+        ctx.cursor.z = zlevel
     end
 end
 
