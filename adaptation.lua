6422266c72fa202a1873853b0099a2102a100994
EarthPulseAcademy <97487051+EarthPulseAcademy@users.noreply.github.com>
2022-12-07 17:55:26 -0500

    Adaptation convert done
    Adaptation convert done

--- adaptation.lua
+++ adaptation.lua
@@ -1,79 +1,128 @@
+-- Original opening comment before lua adaptation
 -- View or set cavern adaptation levels
--- based on adaptation.rb
---[====[
+-- based on removebadthoughts.rb
+
+-- Rewritten by TBSTeun using OpenAI GPT from adaptation.rb
+
+local help = [====[
 
 adaptation
 ==========
 View or set level of cavern adaptation for the selected unit or the whole fort.
 
-Examples:
+Usage::
 
-* Shows cave adaptation of unit under cursor:
-    adaptation
-* Shows cave adaptation of all units:
-    adaptation -all
-* Sets cave adaptation of unit under cursor to 0:
-    adaptation -set 0
+    adaptation <show|set> <him|all> [value]
 
-The ``value`` must be between 0 and 800,000 (inclusive). "set" and "all" arguments may be combined.
+The ``value`` must be between 0 and 800,000 (inclusive).
 
 ]====]
 
-local utils = require('utils')
+-- Color constants, values mapped to color_value enum in include/ColorText.h
+local COLOR_RESET = -1
+local COLOR_GREEN = 2
+local COLOR_RED = 4
+local COLOR_YELLOW = 14
 
-local validArgs = utils.invert({
- 'all',
- 'set',
-})
-
-if moduleMode then
- return
+local function print_color(color, s)
+    dfhack.color(color)
+    dfhack.print(s)
+    dfhack.color(COLOR_RESET)
 end
 
-local args = utils.processArgs({...}, validArgs)
-
-if args.set and (args.set < 0 or args.set > 800000) then
-    print("Value must be between 0 and 800,000.")
-end
-
-local function set_adaptation_value(unit)
-    if unit.flags2.inactive or not dfhack.units.isCitizen(v) then
-        return 0
+local function usage(s)
+    if s ~= nil then
+        print(s)
     end
-    local found = false
-    local amount = 0
-    for k,v in ipairs(unit.status.misc_traits) do
-        if v.id == df.misc_trait_type.CaveAdapt then
-            found = v
+    print("Usage: adaptation <show|set> <him|all> [value]")
+end
+
+local args = {...}
+
+local mode = args[1] or "help"
+local who = args[2]
+local value = args[3]
+
+if mode == "help" then
+    usage()
+elseif mode ~= "show" and mode ~= "set" then
+    usage("Invalid mode '" .. mode .. "': must be either 'show' or 'set'")
+end
+
+if who == nil then
+    usage("Target not specified")
+elseif who ~= "him" and who ~= "all" then
+    usage("Invalid target '" .. who .. "'")
+end
+
+if mode == "set" then
+    if value == nil then
+        usage("Value not specified")
+    elseif not tonumber(value) then
+        usage("Invalid value '" .. value .. "'")
+    end
+
+    if tonumber(value) < 0 or tonumber(value) > 800000 then
+        usage("Value must be between 0 and 800000")
+    end
+    value = tonumber(value)
+
+end
+
+local num_set = 0
+
+local function set_adaptation_value(unit, v)
+    if not dfhack.units.isCitizen(unit) or unit.flags2.killed then
+        return
+    end
+    local trait_found = false
+    for _, t in ipairs(unit.status.misc_traits) do
+        if t.id == df.misc_trait_type.CaveAdapt then
+            if mode == "show" then
+                    print_color(COLOR_RESET, "Unit " .. unit.id .. " (" .. dfhack.TranslateName(dfhack.units.getVisibleName(unit)) .. ") has an adaptation of ")
+                    if t.value <= 399999 then
+                        print_color(COLOR_GREEN, t.value .. "\n")
+                    elseif t.value <= 599999 then
+                        print_color(COLOR_YELLOW, t.value .. "\n")
+                    else
+                        print_color(COLOR_RED, t.value .. "\n")
+                    end
+            elseif mode == "set" then
+                print("Unit " .. unit.id .. " (" .. dfhack.TranslateName(dfhack.units.getVisibleName(unit)).. ") changed from " .. t.value .. " to " .. v)
+                t.value = v
+                num_set = num_set + 1
+            end
+            trait_found = true
             break
         end
     end
-    if not found and args.set then
-        found = df.unit_misc_trait:new()
-        found.id = df.misc_trait_type.CaveAdapt
-        found.value = args.set
-        unit.status.misc_traits:insert('#', found)
+    if not trait_found then
+        if mode == "show" then
+            print_color(COLOR_RESET, "Unit " .. unit.id .. " (" .. dfhack.TranslateName(dfhack.units.getVisibleName(unit)) .. ") has an adaptation of ")
+            print_color(COLOR_GREEN, "0\n")
+        elseif mode == "set" then
+            local new_trait = df.unit_misc_trait:new()
+            new_trait.id = df.misc_trait_type.CaveAdapt
+            new_trait.value = v
+            num_set = num_set + 1
+            table.insert(unit.status.misc_traits, new_trait)
+            print("Unit " .. unit.id .. " (" .. dfhack.TranslateName(dfhack.units.getVisibleName(unit)) .. ") changed from 0 to " .. v)
+        end
     end
-    if found then
-        amount = found.value
-    end
-    if args.set then
-        print("Unit "..dfhack.TranslateName(dfhack.units.getVisibleName(unit)).." changed to "..args.set)
-    else
-        print("Unit "..dfhack.TranslateName(dfhack.units.getVisibleName(unit)).." has an adaptation of "..amount)
-    end
-    return 1
 end
 
-if args.all then
-    for k,v in ipairs(df.global.world.units.all) do
-        set_adaptation_value(v)
-    end
-else
-    local u = dfhack.gui.getSelectedUnit()
+if who == "him" then
+    local u = dfhack.gui.getSelectedUnit(true)
     if u then
-        set_adaptation_value(u)
+        set_adaptation_value(u, value)
     else
-        print("Please select a unit.")
+        print('Please select a dwarf ingame')
+    end
+elseif who == "all" then
+    for _, uu in ipairs(df.global.world.units.all) do
+        set_adaptation_value(uu, value)
+    end
+    if num_set > 0 then
+        print(num_set .. " units changed")
     end
 end
