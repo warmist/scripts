93972fb509afd278215b4b29dfc45e78f1ecdb21
EarthPulseAcademy <97487051+EarthPulseAcademy@users.noreply.github.com>
2022-12-09 15:36:16 -0500

    Processed review comments on adaptation.lua
    Processed review comments on adaptation.lua

--- adaptation.lua
+++ adaptation.lua
@@ -3,6 +3,12 @@
 -- based on removebadthoughts.rb
 -- Rewritten by TBSTeun using OpenAI GPT from adaptation.rb
 
+local args = {...}
+
+local mode = args[1] or 'help'
+local who = args[2]
+local value = args[3]
+
 local function print_color(color, s)
     dfhack.color(color)
     dfhack.print(s)
@@ -11,100 +17,99 @@ end
 
 local function usage(s)
     if s then
-        print(s)
+        dfhack.printerr(s)
     end
     print(dfhack.script_help())
 end
 
 local function set_adaptation_value(unit, v)
-    local num_set = 0
-
-    if not dfhack.units.isCitizen(unit) or unit.flags2.killed then
-        return num_set
+    if not dfhack.units.isCitizen(unit) or not dfhack.units.isAlive(unit) then
+        return 0
     end
-    local trait_found = false
+
     for _, t in ipairs(unit.status.misc_traits) do
         if t.id == df.misc_trait_type.CaveAdapt then
-            if mode == "show" then
-                    print_color(COLOR_RESET, "Unit " .. unit.id .. " (" .. dfhack.TranslateName(dfhack.units.getVisibleName(unit)) .. ") has an adaptation of ")
-                    if t.value <= 399999 then
-                        print_color(COLOR_GREEN, t.value .. "\n")
-                    elseif t.value <= 599999 then
-                        print_color(COLOR_YELLOW, t.value .. "\n")
-                    else
-                        print_color(COLOR_RED, t.value .. "\n")
-                    end
-            elseif mode == "set" then
-                print("Unit " .. unit.id .. " (" .. dfhack.TranslateName(dfhack.units.getVisibleName(unit)).. ") changed from " .. t.value .. " to " .. v)
+            if mode == 'show' then
+                print_color(COLOR_RESET, ('Unit %s (%s) has an adaptation of '):format(unit.id, dfhack.TranslateName(dfhack.units.getVisibleName(unit))))
+                if t.value <= 399999 then
+                    print_color(COLOR_GREEN, ('%s\n'):format(t.value))
+                elseif t.value <= 599999 then
+                    print_color(COLOR_YELLOW, ('%s\n'):format(t.value))
+                else
+                    print_color(COLOR_RED, ('%s\n'):format(t.value))
+                end
+
+                return 0
+            elseif mode == 'set' then
+                print(('Unit %s (%s) changed from %s to %s'):format(unit.id, dfhack.TranslateName(dfhack.units.getVisibleName(unit)), t.value, v))
                 t.value = v
-                num_set = num_set + 1
+                return 1
             end
-            trait_found = true
-            break
         end
     end
-    if not trait_found then
-        if mode == "show" then
-            print_color(COLOR_RESET, "Unit " .. unit.id .. " (" .. dfhack.TranslateName(dfhack.units.getVisibleName(unit)) .. ") has an adaptation of ")
-            print_color(COLOR_GREEN, "0\n")
-        elseif mode == "set" then
-            local new_trait = df.unit_misc_trait:new()
-            new_trait.id = df.misc_trait_type.CaveAdapt
-            new_trait.value = v
-            num_set = num_set + 1
-            table.insert(unit.status.misc_traits, new_trait)
-            print("Unit " .. unit.id .. " (" .. dfhack.TranslateName(dfhack.units.getVisibleName(unit)) .. ") changed from 0 to " .. v)
-        end
+    if mode == 'show' then
+        print_color(COLOR_RESET, ('Unit %s (%s) has an adaptation of '):format(unit.id, dfhack.TranslateName(dfhack.units.getVisibleName(unit))))
+        print_color(COLOR_GREEN, '0\n')
+    elseif mode == 'set' then
+        local new_trait = dfhack.units.getMiscTrait(unit, df.misc_trait_type.CaveAdapt, true)
+        new_trait.id = df.misc_trait_type.CaveAdapt
+        new_trait.value = v
+        print(('Unit %s (%s) changed from 0 to %d'):format(unit.id, dfhack.TranslateName(dfhack.units.getVisibleName(unit)), v))
+        return 1
     end
 
-    return num_set
+    return 0
 end
 
-local args = {...}
-
-local mode = args[1] or "help"
-local who = args[2]
-local value = args[3]
-
-if mode == "help" then
+if mode == 'help' then
     usage()
-elseif mode ~= "show" and mode ~= "set" then
-    usage("Invalid mode '" .. mode .. "': must be either 'show' or 'set'")
+    return
+elseif mode ~= 'show' and mode ~= 'set' then
+    usage(('Invalid mode %s: must be either \'show\' or \'set\''):format(mode))
+    return
 end
 
-if not(who) then
-    usage("Target not specified")
-elseif who ~= "him" and who ~= "all" then
-    usage("Invalid target '" .. who .. "'")
+if not who then
+    usage('Target not specified')
+    return
+elseif who ~= 'him' and who ~= 'all' then
+    usage(('Invalid target %s'):format(who))
+    return
 end
 
-if mode == "set" then
-    if not(value) then
-        usage("Value not specified")
+if mode == 'set' then
+    if not value then
+        usage('Value not specified')
+        return
     elseif not tonumber(value) then
-        usage("Invalid value '" .. value .. "'")
+        usage(('Invalid value %s'):format(value))
+        return
     end
 
-    if tonumber(value) < 0 or tonumber(value) > 800000 then
-        usage("Value must be between 0 and 800000")
-    end
     value = tonumber(value)
-
+    if value < 0 or value > 800000 then
+        usage(('Value must be between 0 and 800000 (inclusive), input value was %s'):format(value))
+    end
 end
 
-if who == "him" then
+local num_set = 0
+
+print("who")
+
+if who == 'him' then
     local u = dfhack.gui.getSelectedUnit(true)
-    local num_set = 0
     if u then
         num_set = set_adaptation_value(u, value)
     else
-        print('Please select a dwarf ingame')
+        dfhack.printerr('Please select a dwarf ingame')
     end
-elseif who == "all" then
+elseif who == 'all' then
+    print("all")
     for _, uu in ipairs(df.global.world.units.all) do
-        num_set = set_adaptation_value(uu, value)
+        num_set = num_set + set_adaptation_value(uu, value)
     end
+
     if num_set > 0 then
-        print(num_set .. " units changed")
+        print(('%s units changed'):format(num_set))
     end
 end
