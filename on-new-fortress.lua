d867d5b80b70d77fc162d417de82f0156dc8af9c
Myk Taylor <myk002@yahoo.com>
2023-01-05 20:31:34 -0800

    update on-new-fortress script help

--- on-new-fortress.lua
+++ on-new-fortress.lua
@@ -1,21 +1,7 @@
 -- runs dfhack commands only in a newborn fortress
 
-local HELP = [====[
-
-on-new-fortress
-===============
-Runs commands like `multicmd`, but only in a newborn fortress.
-
-Use this in ``onMapLoad.init`` with f.e. `ban-cooking`::
-
-  on-new-fortress ban-cooking tallow; ban-cooking honey; ban-cooking oil; ban-cooking seeds; ban-cooking brew; ban-cooking fruit; ban-cooking mill; ban-cooking thread; ban-cooking milk;
-  on-new-fortress 3dveins
-
-]====]
-
-
 if not (...) then
-    print(HELP)
+    print(dfhack.script_help())
     return
 end
 
