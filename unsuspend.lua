1c5cbb63301db60765a1eb1822bbdbabdc9e0f93
Myk Taylor <myk002@yahoo.com>
2023-01-10 01:19:44 -0800

    clean up unsuspend a bit

--- unsuspend.lua
+++ unsuspend.lua
@@ -130,11 +130,12 @@ function SuspendOverlay:refresh_screen_buildings()
     if not self:update_viewport(viewport) then return end
     local screen_buildings, z = {}, viewport.z
     for bld_id,data in pairs(self.in_progress_buildings) do
-        local bld = df.building.find(bld_id) or {}
-        local pos = {x=bld.centerx, y=bld.centery, z=bld.z}
-        if bld.z and viewport:isVisible(pos) then
-            local screen_pos = viewport:tileToScreen(pos)
-            screen_buildings[bld_id] = screen_pos
+        local bld = df.building.find(bld_id)
+        if bld then
+            local pos = {x=bld.centerx, y=bld.centery, z=bld.z}
+            if viewport:isVisible(pos) then
+                screen_buildings[bld_id] = viewport:tileToScreen(pos)
+            end
         end
     end
     self.screen_buildings = screen_buildings
