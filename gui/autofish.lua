f0d888fe474ea5def3880b38d3198e746288ded9
Andriel Chaoti <3628387+AndrielChaoti@users.noreply.github.com>
2023-02-01 10:25:51 -0700

    Various changes
    - change how gui toggles the enable state.
    - make gui window smaller to match size of contents better
    - add more status information
    - changes related to gui functionality and consistent operation.
    - update changelog.
    
    note: renamed the settings variables, if you were using a previous
    testing version, you will need to clear out the persist table for this
    script.

--- gui/autofish.lua
+++ gui/autofish.lua
@@ -2,7 +2,7 @@
 
 local gui = require("gui")
 local widgets = require("gui.widgets")
-local script = reqscript("autofish")
+local autofish = reqscript("autofish")
 
 local REFRESH_MS = 10000
 
@@ -12,7 +12,7 @@ local REFRESH_MS = 10000
 Autofish = defclass(Autofish, widgets.Window)
 Autofish.ATTRS = {
     frame_title = "Autofish",
-    frame = {w=45, h=12},
+    frame = {w=35, h=11},
     resizable = false
 }
 
@@ -25,59 +25,90 @@ function Autofish:init()
             key="CUSTOM_CTRL_E",
             options={{value=true, label="Enabled", pen=COLOR_GREEN},
                      {value=false, label="Disabled", pen=COLOR_RED}},
-            on_change=function(val) script.setEnabled(val) end
+            on_change=function(val) dfhack.run_command{val and "enable" or "disable", "autofish"} end
         },
-        widgets.EditField{
+        --[[widgets.EditField{
             view_id="minimum",
             frame={t=2,l=0},
-            label_text="minimum fish target: ",
+            label_text="Minimum fish target: ",
             key="CUSTOM_M",
             on_char=function(ch) return ch:match("%d") end,
             on_submit=function(text)
-                script.set_minFish = tonumber(text)
+                autofish.set_minFish(tonumber(text))
                 self:refresh_data()
             end
-        },
+        },--]]
         widgets.EditField{
             view_id="maximum",
-            frame={t=3,l=0},
+            frame={t=2,l=0},
             label_text="Maximum fish target: ",
-            key="CUSTOM_SHIFT_M",
+            key="CUSTOM_M",
             on_char=function(ch) return ch:match("%d") end,
             on_submit=function(text)
-                script.set_maxFish = tonumber(text)
+                autofish.set_maxFish(tonumber(text))
+                autofish.set_minFish(math.floor(tonumber(text)*0.75))
                 self:refresh_data()
             end
         },
         widgets.ToggleHotkeyLabel{
             view_id="useRawFish",
-            frame={t=5, l=0, w=31},
+            frame={t=3, l=0, w=31},
             label="Counting raw fish:",
             key="CUSTOM_ALT_R",
             options={{value=true, label="Yes", pen=COLOR_GREEN},
                      {value=false, label="No", pen=COLOR_RED}},
-            on_change=function(val) script.set_useRaw = val end
+            on_change=function(val)
+                autofish.set_useRaw(val)
+            end
+        },
+        widgets.Label{
+            view_id="fish_count",
+            frame={t=5, l=0, h=1},
+            auto_height=false
         },
         widgets.Label{
             view_id="current_mode",
-            frame={t=7, l=0, h=1},
+            frame={t=6, l=0, h=1},
             auto_height=false,
             --visible=false
         }
+
     }
     self:refresh_data()
 end
 
+function get_count_pen(val, min, max)
+    if val <= min then return COLOR_RED end
+    if val <= max then return COLOR_YELLOW end
+    return COLOR_GREEN
+end
+
+function get_count_text(val, min, max)
+    return {text=tostring(val), pen=get_count_pen(val, min, max)}
+end
+
+
 function Autofish:refresh_data()
-    self.subviews.enable_toggle:setOption(script.isEnabled())
+    self.subviews.enable_toggle:setOption(autofish.isEnabled())
 
-    self.subviews.minimum:setText(tostring(script.set_minFish))
-    self.subviews.maximum:setText(tostring(script.set_maxFish))
-    self.subviews.useRawFish:setOption(script.set_useRaw)
+    --self.subviews.minimum:setText(tostring(autofish.set_minFish))
+    self.subviews.maximum:setText(tostring(autofish.s_maxFish))
+    self.subviews.useRawFish:setOption(autofish.s_useRaw)
 
-    self.subviews.current_mode.visible=script.isEnabled()
-    if script.isFishing then
-        self.subviews.current_mode:setText(string.format("Autofish has %s fishing.", (script.isFishing and "enabled" or "disabled")))
+    local prep, raw = autofish.count_fish()
+    local total = prep+raw
+    local counts_text = {
+        "Fish: ",
+        get_count_text(total, autofish.s_minFish, autofish.s_maxFish),
+        " (",
+        {text=tostring(prep), pen=COLOR_GREY}, " prepared, ",
+        {text=tostring(raw), pen=COLOR_GREY}, " raw)"
+    }
+    self.subviews.fish_count:setText(counts_text)
+
+    self.subviews.current_mode.visible=autofish.isEnabled()
+    if autofish.isFishing then
+        self.subviews.current_mode:setText(string.format("Autofish has %s fishing.", (autofish.isFishing and "enabled" or "disabled")))
     end
 
     self.next_refresh_ms = dfhack.getTickCount() + REFRESH_MS
@@ -94,7 +125,7 @@ end
 -- AutofishScreen
 ---
 AutofishScreen = defclass(AutofishScreen, gui.ZScreen)
-AutofishScreen.ATTRS = {focus_path = "autofish"}
+AutofishScreen.ATTRS{focus_path = "autofish"}
 
 function AutofishScreen:init()
     self:addviews{Autofish{}}
