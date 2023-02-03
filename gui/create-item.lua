e43d666f4b96d7043810259ac7a34c9b7b1584ba
ppaawwll <78707934+ppaawwll@users.noreply.github.com>
2023-02-03 02:07:24 +0200

    Update create-item.lua

--- gui/create-item.lua
+++ gui/create-item.lua
@@ -233,7 +233,7 @@ function createCorpsePiece(creator, bodypart, partlayer, creatureID, casteID, ge
  local layerMat
  local tissueID
  local liquid = false
- local isCorpse = bodypart < -2 -- in the hackWish function, the bodypart variable is initialized to -1, which isn't changed if the spawned item is a corpse, but it's passed to this function with two subtracted from it, making it -3 instead
+ local isCorpse = bodypart == -1 -- in the hackWish function, the bodypart variable is initialized to -1, which isn't changed if the spawned item is a corpse
  if not generic and not isCorpse then -- if we have a specified body part and layer, figure all the stuff out about that
  -- store the tissue id of the specific layer we selected
   tissueID = tonumber(creatorBody.body_parts[bodypart].layers[partlayer].tissue_id)
