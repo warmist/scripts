9ff05a29e004133cdf2e118ce6469f9c3b0f629e
Rumrusher <Anuleakage@yahoo.com>
2023-03-13 00:37:59 -0400

    script concept for embark anywhere on DF50
    here's an embark anywhere attempt script this should let one embark anywhere...
    still need gui widget support to handle the new mouse only cursor controls

---
+++ EmbarkAnywhere_v50.lua
@@ -0,0 +1,9 @@
+local MapCure=df.global.gview.view.child
+
+MapCure.warn_mm_startx = MapCure.neighbor_hover_mm_sx
+MapCure.warn_mm_endx = MapCure.neighbor_hover_mm_ex
+MapCure.warn_mm_starty = MapCure.neighbor_hover_mm_sy
+MapCure.warn_mm_endy = MapCure.neighbor_hover_mm_ey
+MapCure.in_embark_salt=true
+-- uhh this is a micro script that functions the basic steps to bypass the embark warnings if anyone wants to update this with gui functions and probably better mosue support feel free. 
+-- not made to replace the old embarkAnywhere command
