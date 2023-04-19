23ec4ad012b920880957b27bacd9f8dd4d1e58c6
humblegar <geir.arne.roos@gmail.com>
2023-04-19 20:58:56 +0200

    Moved doc. Better print.

--- fix/dry-buckets.lua
+++ fix/dry-buckets.lua
@@ -1,16 +1,7 @@
--- Removes water from buckets (for lye-making).
--- Can also be used to remove water (muddy or not) that is blocking well operations.
---[====[
 
-fix/dry-buckets
-===============
-Removes water from all buckets in your fortress, allowing them
-to be used for making lye. Skips buckets being carried, or currently used by a job.
-
-]====]
 
 local emptied = 0
-local inBuilding = 0
+local in_building  = 0
 local water_type = dfhack.matinfo.find('WATER').type
 
 for _,item in ipairs(df.global.world.items.all) do
@@ -22,7 +13,7 @@ for _,item in ipairs(df.global.world.items.all) do
     and item:getType() == df.item_type.LIQUID_MISC
     and not (item.flags.in_job) then
         if container.flags.in_building or item.flags.in_building then
-            inBuilding = inBuilding + 1
+            in_building  = in_building  + 1
         end        
         dfhack.items.remove(item)
         emptied = emptied + 1
@@ -31,5 +22,5 @@ end
 
 print('Emptied '..emptied..' buckets.')
 if emptied > 0 then
-    print(''..inBuilding..' of those were in a building.')    
+    print(('Unclogged %d wells.'):format(in_building))   
 end
