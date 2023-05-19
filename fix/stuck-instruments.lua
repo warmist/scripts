07cedb1290bcfd292dd6518ff5b0b0519e6d07ed
Myk <myk.taylor@gmail.com>
2023-05-19 12:50:50 -0700

    Apply suggestions from code review

--- fix/stuck-instruments.lua
+++ fix/stuck-instruments.lua
@@ -1,47 +1,20 @@
 -- Fixes instruments that never got played during a performance
 
-local help = [====[
+local argparse = require('argparse')
 
-fix/stuck-instruments
-=====================
-
-Fixes instruments that were picked up for a performance, but were instead
-simulated and are now stuck permanently in a job that no longer exists.
-
-This works around the issue encountered with :bug:`9485`, and should be run
-if you notice any instruments lying on the ground that seem to be stuck in a
-job.
-
-Run ``fix/stuck-instruments -n`` or ``fix/stuck-instruments --dry-run`` to
-list how many instruments would be fixed without performing the action.
-
-]====]
-
-
-function fixInstruments(args)
-    local dry_run = false
+function fixInstruments(opts)
     local fixed = 0
-    for _, arg in pairs(args) do
-        if args[1]:match('-h') or args[1]:match('help') then
-            print(dfhack.script_help())
-            return
-        elseif args[1]:match('-n') or args[1]:match('dry') then
-            dry_run = true
-        end
-    end
 
     for _, item in ipairs(df.global.world.items.other.INSTRUMENT) do
         for i, ref in pairs(item.general_refs) do
             if ref:getType() == df.general_ref_type.ACTIVITY_EVENT then
                 local activity = df.activity_entry.find(ref.activity_id)
                 if not activity then
-                    if not dry_run then
+                    if not opts.dry_run then
                         --remove dead activity reference
+                        item.general_refs[i]:delete()
                         item.general_refs:erase(i)
-                        if item.flags.in_job then
-                            --remove stuck in_job flag if true
-                            item.flags.in_job = false
-                        end
+                        item.flags.in_job = false
                     end
                     fixed = fixed + 1
                     break
@@ -50,7 +23,7 @@ function fixInstruments(args)
         end
     end
 
-    if fixed > 0 or dry_run then
+    if fixed > 0 or opts.dry_run then
         print(("%s %d stuck instruments."):format(
                 dry_run and "Found" or "Fixed",
                 fixed
@@ -58,7 +31,16 @@ function fixInstruments(args)
     end
 end
 
+local opts = {}
 
-if not dfhack_flags.module then
-    fixInstruments{...}
+local positionals = argparse.processArgsGetopt({...}, {
+    { 'h', 'help', handler = function() options.help = true end },
+    { 'n', 'dry-run', handler = function() options.dry_run = true end },
+})
+
+if positionals[1] == 'help' or opts.help then
+    print(dfhack.script_help())
+    return
 end
+
+fixInstruments(opts)
