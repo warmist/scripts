5e1827c84751347ad8f91f3b11771e10b9ae089f
master-spike <najeeb18@gmail.com>
2023-07-11 20:24:34 +0100

    added script description in comments

--- fix/empty-wheelbarrows.lua
+++ fix/empty-wheelbarrows.lua
@@ -1,5 +1,10 @@
+--checks all wheelbarrows on map for rocks stuck in them. If a wheelbarrow isn't in use for a job (hauling) then there should be no rocks in them
+--rocks will occasionally get stuck in wheelbarrows, and accumulate if the wheelbarrow gets used.
+--this script empties all wheelbarrows which have rocks stuck in them.
+
 
 for _,e in ipairs(df.global.world.items.other.TOOL) do
+    -- wheelbarrow must be on ground and not in a job
     if ((not e.flags.in_job) and e.flags.on_ground) then
         if e.subtype.id == "ITEM_TOOL_WHEELBARROW" then
             local items = dfhack.items.getContainedItems(e)
