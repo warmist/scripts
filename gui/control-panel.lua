de97384f07f05bb50714ddb737b01e8e3dcd62ee
master-spike <najeeb18@gmail.com>
2023-07-13 23:44:14 +0100

    fixed typo (stuck -> empty)

--- gui/control-panel.lua
+++ gui/control-panel.lua
@@ -120,7 +120,8 @@ local REPEATS = {
         desc='Show a warning dialog when units are starving or dehydrated.',
         command={'--time', '10', '--timeUnits', 'days', '--command', '[', 'warn-starving', ']'}},
     ['empty-wheelbarrows']={
-        command={'--time', '1', '--timeUnits', 'days', '--command', '[', 'fix/stuck-wheelbarrows', '-q', ']'}},
+        desc='Empties wheelbarrows which have rocks stuck in them.',
+        command={'--time', '1', '--timeUnits', 'days', '--command', '[', 'fix/empty-wheelbarrows', '-q', ']'}},
 }
 local REPEATS_LIST = {}
 for k in pairs(REPEATS) do
