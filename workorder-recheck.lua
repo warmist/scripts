fcb7a53dfcf18818f849bcda8757972a7a22b246
Timur Kelman <tymur.gubayev@gmail.com>
2023-08-12 20:21:16 +0200

    remove doc-string from workorder-recheck.lua

--- workorder-recheck.lua
+++ workorder-recheck.lua
@@ -1,11 +1,4 @@
 -- Resets the selected work order to the `Checking` state
---[====[
-
-workorder-recheck
-=================
-Sets the status to ``Checking`` (from ``Active``) of the selected work order.
-This makes the manager reevaluate its conditions.
-]====]
 
 --@ module = true
 
