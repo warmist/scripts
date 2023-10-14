e626d72a386e9a558f22530369487920a3cc971a
Branislav Setlák <branislav.setlak@student.tuke.sk>
2023-10-14 15:01:22 +0200

    fixed trailing whitespace

--- fix/corrupt-jobs.lua
+++ fix/corrupt-jobs.lua
@@ -3,10 +3,10 @@
 local count = 0
 
 for _, unit in ipairs(df.global.world.units.all) do
-    if unit.job.current_job and unit.job.current_job.id == -1 then 
+    if unit.job.current_job and unit.job.current_job.id == -1 then
         unit.job.current_job = nil
         count = count + 1
-    end 
+    end
 end
 
 if count > 0 then
