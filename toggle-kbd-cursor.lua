8fe7c969cc0cead09ded191c1a3c7aaf53f8e4be
Myk Taylor <myk002@yahoo.com>
2023-11-13 19:39:31 -0800

    clear cursor position when disabling cursor

--- toggle-kbd-cursor.lua
+++ toggle-kbd-cursor.lua
@@ -4,6 +4,7 @@ local flags4 = df.global.d_init.flags4
 
 if flags4.KEYBOARD_CURSOR then
     flags4.KEYBOARD_CURSOR = false
+    guidm.setCursorPos(xyz2pos(-30000, -30000, -30000))
     print('Keyboard cursor disabled.')
 else
     guidm.setCursorPos(guidm.Viewport.get():getCenter())
