8f561a93ae66318a3ebee70732ac0d005acfb76b
Myk Taylor <myk002@yahoo.com>
2024-01-03 13:00:32 -0800

    ajust code to use new field names

--- docs/reveal-hidden-sites.rst
+++ docs/reveal-hidden-sites.rst
@@ -3,12 +3,12 @@ reveal-hidden-sites
 
 .. dfhack-tool::
     :summary: Reveal all sites in the world.
-    :tags: adventure fort armok map
+    :tags: adventure embark fort armok map
 
 This tool reveals all sites in the world that have yet to be discovered by the
 player (camps, lairs, shrines, vaults, etc), making them visible on the map.
 
-It is usable in both fortress and adventure mode.
+It is usable in both fortress and adventure mode, or on the embark map.
 
 Please see `reveal-adv-map` if you also want to expose hidden world map tiles in
 adventure mode.
--- reveal-hidden-sites.lua
+++ reveal-hidden-sites.lua
@@ -1,26 +1,7 @@
--- Expose all undiscovered sites.
--- author: Atomic Chicken
-
---[====[
-
-reveal-hidden-sites
-===================
-This script reveals all sites in the world
-that have yet to be discovered by the player
-(camps, lairs, shrines, vaults, etc)
-thus making them visible on the map.
-
-Usable in both fortress and adventure mode.
-
-See `reveal-adv-map` if you also want to expose
-hidden world map tiles in adventure mode.
-
-]====]
-
 local count = 0
 for _, site in ipairs(df.global.world.world_data.sites) do
-  if site.flags.Undiscovered then
-    site.flags.Undiscovered = false
+  if site.flag.HIDDEN then
+    site.flag.HIDDEN = false
     count = count + 1
   end
 end
