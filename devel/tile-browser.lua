a1f7bb68b9f5e705c95bf453dc4602e490947948
Myk Taylor <myk002@yahoo.com>
2024-03-07 16:53:25 -0800

    simplify code now that we don't have to handle large textures
    SDL2 normalizes the size of textures; we only have to handle 1x1

--- devel/tile-browser.lua
+++ devel/tile-browser.lua
@@ -1,77 +1,72 @@
 local gui = require('gui')
 local widgets = require('gui.widgets')
 
+local raws = df.global.enabler.textures.raws
+
 TileBrowser = defclass(TileBrowser, gui.ZScreen)
 TileBrowser.ATTRS{
     focus_string='tile-browser',
 }
 
 function TileBrowser:init()
-    self.max_texpos = #df.global.enabler.textures.raws
-    self.tile_size = 1
+    self.dirty = true
 
     local main_panel = widgets.Window{
-        view_id='main',
-        frame={w=32, h=30},
-        drag_anchors={title=true, body=true},
+        frame_title='Tile Browser',
+        frame={w=35, h=30},
         resizable=true,
         resize_min={h=20},
-        on_layout=self:callback('on_resize'),
-        frame_title='Tile Browser',
     }
 
     main_panel:addviews{
         widgets.EditField{
             view_id='start_index',
             frame={t=0, l=0},
-            key='CUSTOM_CTRL_A',
+            key='CUSTOM_ALT_S',
             label_text='Start index: ',
             text='0',
             on_submit=self:callback('set_start_index'),
-            on_char=function(ch) return ch:match('%d') end},
-        widgets.HotkeyLabel{
-            frame={t=1, l=0},
-            label='Prev',
-            key='KEYBOARD_CURSOR_UP_FAST',
-            on_activate=self:callback('shift_start_index', -1000)},
-        widgets.Label{
-            frame={t=1, l=6, w=1},
-            text={{text=string.char(24), pen=COLOR_LIGHTGREEN}}},
-        widgets.HotkeyLabel{
-            frame={t=1, l=15},
-            label='Next',
-            key='KEYBOARD_CURSOR_DOWN_FAST',
-            on_activate=self:callback('shift_start_index', 1000)},
-        widgets.Label{
-            frame={t=1, l=21, w=1},
-            text={{text=string.char(25), pen=COLOR_LIGHTGREEN}}},
+            on_char=function(ch) return ch:match('%d') end,
+        },
         widgets.Label{
             view_id='header',
-            frame={t=3, l=0, r=2}},
-        widgets.Panel{
-            view_id='transparent_area',
-            frame={t=4, b=4, l=5, r=2},
-            frame_background=gui.TRANSPARENT_PEN},
+            frame={t=2, l=8},
+            text='0123456789 0123456789',
+        },
         widgets.Label{
             view_id='report',
-            frame={t=4, b=4},
-            scroll_keys={
-                STANDARDSCROLL_UP = -1,
-                KEYBOARD_CURSOR_UP = -1,
-                STANDARDSCROLL_DOWN = 1,
-                KEYBOARD_CURSOR_DOWN = 1,
-                STANDARDSCROLL_PAGEUP = '-page',
-                STANDARDSCROLL_PAGEDOWN = '+page'}},
+            frame={t=3, b=3, l=0},
+            auto_height=false,
+        },
         widgets.Label{
             view_id='footer',
-            frame={b=3, l=0, r=2}},
-        widgets.WrappedLabel{
-            frame={b=0},
-            text_to_wrap='Please resize window to change visible tile size.'},
+            frame={b=2, l=8},
+            text='0123456789 0123456789',
+        },
+        widgets.HotkeyLabel{
+            frame={b=0, l=0},
+            label='Prev',
+            key='KEYBOARD_CURSOR_UP_FAST',
+            auto_width=true,
+            on_activate=self:callback('shift_start_index', -1000),
+        },
+        widgets.Label{
+            frame={b=0, l=6, w=1},
+            text={{text=string.char(24), pen=COLOR_LIGHTGREEN}},
+        },
+        widgets.HotkeyLabel{
+            frame={b=0, l=18},
+            label='Next',
+            key='KEYBOARD_CURSOR_DOWN_FAST',
+            auto_width=true,
+            on_activate=self:callback('shift_start_index', 1000),
+        },
+        widgets.Label{
+            frame={b=0, l=24, w=1},
+            text={{text=string.char(25), pen=COLOR_LIGHTGREEN}},
+        },
     }
     self:addviews{main_panel}
-
-    self:set_start_index('0')
 end
 
 function TileBrowser:shift_start_index(amt)
@@ -82,57 +77,32 @@ function TileBrowser:set_start_index(idx)
     idx = tonumber(idx)
     if not idx then return end
 
-    idx = math.max(0, math.min(self.max_texpos - 980, idx))
+    idx = math.max(0, math.min(#raws - 980, idx))
 
     idx = idx - (idx % 20) -- floor to nearest multiple of 20
     self.subviews.start_index:setText(tostring(idx))
-    self.subviews.transparent_area.frame.l = #tostring(idx) + 4
     self.dirty = true
 end
 
 function TileBrowser:update_report()
     local idx = tonumber(self.subviews.start_index.text)
-    local end_idx = math.min(self.max_texpos, idx + 999)
-    local prefix_len = #tostring(idx) + 4
-
-    local guide = {}
-    table.insert(guide, {text='', width=prefix_len})
-    for n=0,9 do
-        table.insert(guide, {text=tostring(n), width=self.tile_size})
-    end
-    table.insert(guide, {text='', width=self.tile_size})
-    for n=0,9 do
-        table.insert(guide, {text=tostring(n), width=self.tile_size})
-    end
-    self.subviews.header:setText(guide)
-    self.subviews.footer:setText(guide)
+    local end_idx = math.min(#raws-1, idx+999)
 
     local report = {}
     for texpos=idx,end_idx do
         if texpos % 20 == 0 then
-            table.insert(report, {text=tostring(texpos), width=prefix_len})
+            table.insert(report, {text=texpos, width=7, rjustify=true})
+            table.insert(report, ' ')
         elseif texpos % 10 == 0 then
-            table.insert(report, {text='', pad_char=' ', width=self.tile_size})
+            table.insert(report, ' ')
         end
-        table.insert(report, {tile=texpos, pen=gui.KEEP_LOWER_PEN, width=self.tile_size})
+        table.insert(report, {tile=texpos, pen=gui.KEEP_LOWER_PEN, width=1})
         if (texpos+1) % 20 == 0 then
-            for n=1,self.tile_size do
-                table.insert(report, NEWLINE)
-            end
+            table.insert(report, NEWLINE)
         end
     end
 
     self.subviews.report:setText(report)
-    self.subviews.main:updateLayout()
-end
-
-function TileBrowser:on_resize(body)
-    local report_width = body.width - 15
-    local prev_tile_size = self.tile_size
-    self.tile_size = (report_width // 21) + 1
-    if prev_tile_size ~= self.tile_size then
-        self.dirty = true
-    end
 end
 
 function TileBrowser:onRenderFrame()
--- docs/devel/tile-browser.rst
+++ docs/devel/tile-browser.rst
@@ -9,10 +9,9 @@ This script pops up a panel that shows a page of 1000 textures. You can change
 the starting texpos index with :kbd:`Ctrl`:kbd:`A` or scan forward or backwards
 in increments of 1000 with :kbd:`Shift` :kbd:`Up`/:kbd:`Down`.
 
-Textures that take up more than one grid position may have only their upper-left
-tiles shown. Increase the tile browser window (drag the bottom right corner to
-resize) to see larger tiles Note there may be transparent space visible through
-the window for tiles that don't take up the entire allotted space.
+Textures are resized to the dimensions of the interface grid (8px by 12px), so
+map textures (nominally 32px by 32px) will be squished. If no texture is
+assigned to the texpos index, the window will be transparent at that spot.
 
 Usage
 -----
