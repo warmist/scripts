f8937dc6bfdc605a92056a1b9c184c80e2791487
Myk Taylor <myk002@yahoo.com>
2024-03-12 20:04:01 -0700

    remove old docstring

--- migrants-now.lua
+++ migrants-now.lua
@@ -1,14 +1,5 @@
 -- Force a migrant wave (only after hardcoded waves)
---[====[
 
-migrants-now
-============
-Forces an immediate migrant wave. Only works after migrants have
-arrived naturally. Roughly equivalent to `modtools/force` with::
-
-    modtools/force -eventType migrants
-
-]====]
 df.global.timed_events:insert('#',{
     new = true,
     type = df.timed_event_type.Migrants,
