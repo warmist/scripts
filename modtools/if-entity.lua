cf3d8927be4b4ebb634bf90e86ec361911d0aec4
Myk Taylor <myk002@yahoo.com>
2024-03-14 11:37:06 -0700

    update modtools/if-entity

--- modtools/if-entity.lua
+++ modtools/if-entity.lua
@@ -1,38 +1,6 @@
 -- Run a command if the current entity matches a given ID.
 
---[[
-Consider this public domain (CC0).
-    - Milo Christiansen
-]]
-
-local usage = [====[
-modtools/if-entity
-==================
-
-Run a command if the current entity matches a given ID.
-
-To use this script effectively it needs to be called from "raw/onload.init".
-Calling this from the main dfhack.init file will do nothing, as no world has
-been loaded yet.
-
-Usage:
-
-- ``id``:
-    Specify the entity ID to match
-- ``cmd [ commandStrs ]``:
-    Specify the command to be run if the current entity matches the entity
-    given via -id
-
-All arguments are required.
-
-Example:
-
-- Print a message if you load an elf fort, but not a dwarf, human, etc. fort::
-
-    if-entity -id "FOREST" -cmd [ lua "print('Dirty hippies.')" ]
-]====]
-
-local utils = require 'utils'
+local utils = require('utils')
 
 local validArgs = utils.invert({
     'help',
@@ -43,14 +11,18 @@ local validArgs = utils.invert({
 local args = utils.processArgs({...}, validArgs)
 
 if not args.id or not args.cmd or args.help then
-    dfhack.print(usage)
+    print(dfhack.script_help())
     return
 end
 
+if df.global.gamemode ~= df.game_mode.DWARF or not dfhack.isMapLoaded() then
+    error('emigration needs a loaded fortress map to work')
+end
+
+
 local entsrc = df.historical_entity.find(df.global.plotinfo.civ_id)
-if entsrc == nil then
-    dfhack.printerr("Could not find current entity. No world loaded?")
-    return
+if not entsrc then
+    error('could not find current entity')
 end
 
 if entsrc.entity_raw.code == args.id then
