dc7f4b37bea732e327d73cd2dd70fe8eb0674811
terribleperson <terribleperson@terribleperson.net>
2024-03-20 20:55:32 -0400

    Update zero-noexert-exhaustion.lua

--- fix/zero-noexert-exhaustion.lua
+++ fix/zero-noexert-exhaustion.lua
@@ -4,22 +4,22 @@
 --Individual Combat Training seems to add 50 Exhaustion approximately every 9 ticks. 'Tired' appears at 2000 Exhaustion.
 
 function isNoExert(u)
-	if(u.curse.rem_tags1.NOEXERT) then --tag removal overrides tag addition, so if the NOEXERT tag is removed the unit cannot be NOEXERT.
-		return false
-	end
-	if(u.curse.add_tags1.NOEXERT) then--if the tag hasn't been removed, and the unit has a curse that adds it, they must be NOEXERT.
-		return true
-	end
-	if(dfhack.units.casteFlagSet(u.race,u.caste, df.caste_raw_flags.NOEXERT)) then --if the tag hasn't been added or removed, but their race and caste has the tag, they're NOEXERT.
-		return true
-	end
+    if(u.curse.rem_tags1.NOEXERT) then --tag removal overrides tag addition, so if the NOEXERT tag is removed the unit cannot be NOEXERT.
+        return false
+    end
+    if(u.curse.add_tags1.NOEXERT) then--if the tag hasn't been removed, and the unit has a curse that adds it, they must be NOEXERT.
+        return true
+    end
+    if(dfhack.units.casteFlagSet(u.race,u.caste, df.caste_raw_flags.NOEXERT)) then --if the tag hasn't been added or removed, but their race and caste has the tag, they're NOEXERT.
+        return true
+    end
 end
 
 function fixNoExertExhaustion()
-	for _, unit in ipairs(df.global.world.units.active) do
+    for _, unit in ipairs(df.global.world.units.active) do
         if(isNoExert(unit)) then
-			unit.counters2.exhaustion = 0	-- 0 represents no Exhaustion. NOEXERT units should never have Exhaustion above 0.
-	end
+            unit.counters2.exhaustion = 0    -- 0 represents no Exhaustion. NOEXERT units should never have Exhaustion above 0.
+        end
     end
 end
 
