da1ff5a9afb2ad51d90f27173436c42fce7bec73
Myk Taylor <myk002@yahoo.com>
2024-04-12 22:03:40 -0700

    cleanup

--- modtools/moddable-gods.lua
+++ modtools/moddable-gods.lua
@@ -1,31 +1,6 @@
--- Create gods from the command-line
---based on moddableGods by Putnam
---edited by expwnent
-local usage = [====[
+local utils = require('utils')
 
-modtools/moddable-gods
-======================
-This is a standardized version of Putnam's moddableGods script. It allows you
-to create gods on the command-line.
-
-Arguments::
-
-    -name godName
-        sets the name of the god to godName
-        if there's already a god of that name, the script halts
-    -spheres [ sphereList ]
-        define a space-separated list of spheres of influence of the god
-    -gender male|female|neuter
-        sets the gender of the god
-    -depictedAs str
-        often depicted as a str
-    -verbose
-        if specified, prints details about the created god
-
-]====]
-local utils = require 'utils'
-
-local validArgs = utils.invert({
+local validArgs = utils.invert{
  'help',
  'name',
  'spheres',
@@ -33,11 +8,11 @@ local validArgs = utils.invert({
  'depictedAs',
  'verbose',
 -- 'entities',
-})
+}
 local args = utils.processArgs({...}, validArgs)
 
 if args.help then
- print(usage)
+ print(dfhack.script_help())
  return
 end
 
